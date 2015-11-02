9e501260dd1c6a8ecd1ff9813d6ccb8fa8945fd5
Sahakorn Buangam <sahakorn.new@outlook.com>
2015-11-02 12:14:34 +0700

    Config Sahakorn
    config to sahakro

--- ComputerProject/website/Login.aspx.cs
+++ ComputerProject/website/Login.aspx.cs
@@ -10,6 +10,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Newtonsoft.Json;
+using System.Web.Security;
 
 namespace ComputerProject.website
 {
@@ -47,7 +48,7 @@ namespace ComputerProject.website
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+        
         }
 
         protected void submit_login_Click(object sender, EventArgs e)
